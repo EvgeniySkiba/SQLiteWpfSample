9221c1368bb2dbd21b4db3b4e0fb8128de876cfe
skiba.evgeniy <skiba.Evgeniy@hotmail.com>
2017-08-18 18:52:53 +0300

    MVVM и SQLite

---
+++ ApplicationViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Runtime.CompilerServices;
+
+namespace SQLiteWpfSample
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        ApplicationContext db;
+        RelayCommand addCommand;
+        RelayCommand editCommand;
+        RelayCommand deleteCommand;
+        IEnumerable<Phone> phones;
+
+        public IEnumerable<Phone> Phones
+        {
+            get { return phones; }
+            set
+            {
+                phones = value;
+                OnPropertyChanged("Phones");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            db = new ApplicationContext();
+            db.Phones.Load();
+            // загружаем данные из бд в локальный кэш:
+            Phones = db.Phones.Local.ToBindingList();
+        }
+        // команда добавления
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand((o) =>
+                  {
+                      PhoneWindow phoneWindow = new PhoneWindow(new Phone());
+                      if (phoneWindow.ShowDialog() == true)
+                      {
+                          Phone phone = phoneWindow.Phone;
+                          db.Phones.Add(phone);
+                          db.SaveChanges();
+                      }
+                  }));
+            }
+        }
+        // команда редактирования
+        public RelayCommand EditCommand
+        {
+            get
+            {
+                return editCommand ??
+                  (editCommand = new RelayCommand((selectedItem) =>
+                  {
+                      if (selectedItem == null) return;
+                      // получаем выделенный объект
+                      Phone phone = selectedItem as Phone;
+
+                      Phone vm = new Phone()
+                      {
+                          Id = phone.Id,
+                          Company = phone.Company,
+                          Price = phone.Price,
+                          Title = phone.Title
+                      };
+                      PhoneWindow phoneWindow = new PhoneWindow(vm);
+
+
+                      if (phoneWindow.ShowDialog() == true)
+                      {
+                          // получаем измененный объект
+                          phone = db.Phones.Find(phoneWindow.Phone.Id);
+                          if (phone != null)
+                          {
+                              phone.Company = phoneWindow.Phone.Company;
+                              phone.Title = phoneWindow.Phone.Title;
+                              phone.Price = phoneWindow.Phone.Price;
+                              db.Entry(phone).State = EntityState.Modified;
+                              db.SaveChanges();
+                          }
+                      }
+                  }));
+            }
+        }
+        // команда удаления
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ??
+                  (deleteCommand = new RelayCommand((selectedItem) =>
+                  {
+                      if (selectedItem == null) return;
+                      // получаем выделенный объект
+                      Phone phone = selectedItem as Phone;
+                      db.Phones.Remove(phone);
+                      db.SaveChanges();
+                  }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 
 namespace SQLiteWpfSample
 {
@@ -10,65 +7,11 @@ namespace SQLiteWpfSample
     /// </summary>
     public partial class MainWindow : Window
     {
-        ApplicationContext db;
         public MainWindow()
         {
             InitializeComponent();
 
-            db = new ApplicationContext();
-            db.Phones.Load();
-            this.DataContext = db.Phones.Local.ToBindingList();
-        }
-        // добавление
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            PhoneWindow phoneWindow = new PhoneWindow(new Phone());
-            if (phoneWindow.ShowDialog() == true)
-            {
-                Phone phone = phoneWindow.Phone;
-                db.Phones.Add(phone);
-                db.SaveChanges();
-            }
-        }
-        // редактирование
-        private void Edit_Click(object sender, RoutedEventArgs e)
-        {
-            // если ни одного объекта не выделено, выходим
-            if (phonesList.SelectedItem == null) return;
-            // получаем выделенный объект
-            Phone phone = phonesList.SelectedItem as Phone;
-
-            PhoneWindow phoneWindow = new PhoneWindow(new Phone
-            {
-                Id = phone.Id,
-                Company = phone.Company,
-                Price = phone.Price,
-                Title = phone.Title
-            });
-
-            if (phoneWindow.ShowDialog() == true)
-            {
-                // получаем измененный объект
-                phone = db.Phones.Find(phoneWindow.Phone.Id);
-                if (phone != null)
-                {
-                    phone.Company = phoneWindow.Phone.Company;
-                    phone.Title = phoneWindow.Phone.Title;
-                    phone.Price = phoneWindow.Phone.Price;
-                    db.Entry(phone).State = EntityState.Modified;
-                    db.SaveChanges();
-                }
-            }
-        }
-        // удаление
-        private void Delete_Click(object sender, RoutedEventArgs e)
-        {
-            // если ни одного объекта не выделено, выходим
-            if (phonesList.SelectedItem == null) return;
-            // получаем выделенный объект
-            Phone phone = phonesList.SelectedItem as Phone;
-            db.Phones.Remove(phone);
-            db.SaveChanges();
+            this.DataContext = new ApplicationViewModel();
         }
     }
 }
